d1938d75adcdc7b3331df81274dad96edfdc2c49
Jackson Stark <jacksonstark77@hotmail.com>
2020-02-03 14:04:27 -0800

    day_8.js SOLVED

--- day_8.js
+++ day_8.js
@@ -7,6 +7,39 @@
 // domainName("http://www.zombie-bites.com") == "zombie-bites"
 // domainName("https://www.cnet.com") == "cnet"
 
-function domainName(url) {
+function domainName(url){
+  let array = url.split('')
+  let type = ''
   
+  array.map((x,i) => x === '.')
+  array[0] === 'w' ? type = 'www' : type = 'http'
+  array[0] !== 'w' && (array[0] !== 'h' && array[1] !== 'h') ? type = 'direct' : null
+  
+  if (type === 'www') {
+    array.splice(0,4)
+    array.map((x,i) => x === '.' ? array = array.slice(0,i) : null)
+  }
+  
+  if (type === 'http') {
+    array.splice(0,7)
+    // if it was https instead of http, we'll have a '/' left over
+    array[0] === '/' ? array.shift() : null
+    array[0] === 'w' && array[1] === 'w' ? array = domainName(array.join('')).split('') : null
+    array.map((x,i) => x === '.' ? array = array.slice(0,i) : null)
+  }
+
+  if (type === 'direct') {
+    array.map((x,i) => x === '.' ? array = array.slice(0,i) : null)
+  }
+
+  return array.join('')
+
 }
+
+domainName('www.youtube.com')
+
+
+
+// www.
+// http://
+// https://
