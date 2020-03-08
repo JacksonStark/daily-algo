7b92faa0c7ce4878f945631afd43d8090265556c
Jackson Stark <jacksonstark77@hotmail.com>
2020-03-08 14:36:51 -0600

    day_11.js SOLVED

--- day_11.js
+++ day_11.js
@@ -6,6 +6,27 @@
 // pigIt('Pig latin is cool');    // igPay atinlay siay oolcay
 // pigIt('Hello world !');        // elloHay orldway !
 
-function pigIt() {
-  
+function pigIt(str) {
+
+  let splitSentence = str.split(' ');
+  let result = splitSentence.map((word, index) => {
+    let splitWord = word.split('');
+
+    // add first character to end
+    splitWord.push(splitWord[0]);
+
+    // delete first character after adding it to end
+    splitWord.shift();
+
+    // check to not add 'ay' onto ending punctuation
+    let ending = splitWord.length === 1 && index === splitSentence.length - 1 ? '' : 'ay';
+
+    return splitWord.join('') + ending;
+  });
+
+  return result.join(' ');
 }
+
+console.log("RESULT ✅ |>  ", pigIt("Pig latin is a cool thing !"));
+
+var re = /(?:\d{3}|\(\d{3}\))([-\/\.])\d{3}\1\d{4}/;
